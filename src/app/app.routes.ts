b4c457843e1216b47ca8f4cafeda31c077dd6814
MyCodeClub <ankitsahay1990@gmail.com>
2025-04-19 08:55:42 +0530

    Initial Commit for MyRestroNgApp settingup layout...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contactus', component: ContactusComponent },
+  { path: 'login', component: LoginComponent },
+];
